42acd4e5799e090a2f01033ed05cd388dc2157f3
k123321141 <kk123321141@gmail.com>
2021-01-26 17:40:09 +0800

    152 94% v2

--- 152.maximum_product_subarray.py
+++ 152.maximum_product_subarray.py
@@ -4,48 +4,56 @@ class Solution:
     If array contains zero, seperatre array by zero and find the maximum product of each array.
     '''
     def maxProduct(self, nums: List[int]) -> int: # noqa
-        # recusive 34%
+        # one pass 94%
         if len(nums) == 0:
             return 0
         elif len(nums) == 1:
             return nums[0]
         # split by zero
         i = 0
-        zero_flag = False
-        ans_arr = []
+        while nums[i] == 0:
+            i += 1
 
-        for j in range(len(nums)):
+        ans = 0 if i > 0 else float('-inf')
+        product = left = right = nums[i]
+
+        j = i+1
+        while j < len(nums):
             if nums[j] == 0:
-                zero_flag = True
-                if j-i > 0:
-                    ans_arr.append(self.foo(nums[i:j]))
+                leng = j-i
+                ans = 0 if 0 > ans else ans
+                if leng == 1:
+                    ans = nums[i] if nums[i] > ans else ans
+                elif leng > 1:
+                    if product > 0:
+                        ans = product if product > ans else ans
+                    else:
+                        # ret = self.foo(nums[i:j], product, left)
+                        ret = product // (left if left >  right else right)
+                        ans = ret if ret > ans else ans
                 i = j+1
+                while i < len(nums) and nums[i] == 0:
+                    i += 1
+                if i < len(nums):
+                    product = left = right = nums[i]
+
+                j = i+1
+            else:
+                product *= nums[j]
+                if left > 0:
+                    left = product
+                if nums[j] < 0:
+                    right = nums[j]
+                else:
+                    right *= nums[j]
+                j += 1
+
         if nums[-1] != 0:
-            ans_arr.append(self.foo(nums[i:]))
-        if zero_flag:
-            ans_arr.append(0)
-        return max(ans_arr)
-
-    def foo(self, nums: List[int]) -> int: # noqa
-        if len(nums) == 1:
-            return nums[0]
-        product = 1
-        for i, n in enumerate(nums):
-            product *= n
-        if product > 0:
-            return product
-        else:
-            # find the less tail
-            left = nums[0]
-            i = 1
-            while left > 0:
-                left *= nums[i]
-                i += 1
-
-            right = nums[-1]
-            i = len(nums)-2
-            while right > 0:
-                right *= nums[i]
-                i -= 1
-            # print(product, left, right)
-            return product // max(left, right)
+            if i == len(nums)-1:
+                ans = nums[i] if nums[i] > ans else ans
+            elif product > 0:
+                ans = product if product > ans else ans
+            else:
+                ret = product // max(left, right)
+                ans = ret if ret > ans else ans
+        return ans
