e8fc9ce0ae56190e8c34d3c44a9b1d00d10c1d72
k123321141 <kk123321141@gmail.com>
2021-02-05 18:25:02 +0800

    198, 99%

--- 198.house_robber.py
+++ 198.house_robber.py
@@ -1,10 +1,7 @@
-from functools import lru_cache
-
-
 class Solution:
     def rob(self, nums: List[int]) -> int: # noqa
         '''
-        DP vresion 25%
+        iterative version 86%
         Keep tracking index that can make max sum for nums[idx:] array.
 
         '''
@@ -15,16 +12,8 @@ class Solution:
         elif len(nums) == 2:
             return max(nums)
         else:
-            self.nums = nums
-            return max([self.dp(i) for i in range(len(nums))])
+            nums[-2] = nums[-1] if nums[-1] > nums[-2] else nums[-2]
 
-    @lru_cache
-    def dp(self, idx: int):
-        if idx == len(self.nums) - 1:
-            ret = max(0, self.nums[idx])
-        elif idx == len(self.nums) - 2:
-            ret = max(0, self.nums[idx], self.nums[idx+1])
-        else:
-            ret = max(0, *[self.nums[idx] + self.dp(i) for i in range(idx+2, len(self.nums))])
-        # print(f'idx: {idx}, ret: {ret}')
-        return ret
+            for i in range(len(nums)-3, -1, -1):
+                nums[i] = max(nums[i] + nums[i+2], nums[i+1])
+            return nums[0]
