07c7a1e824c3c33f188024a240d819aa6624946a
k123321141 <kk123321141@gmail.com>
2021-03-17 14:39:42 +0800

    309, 34%

--- 309.best_time_to_buy_and_sell_stock_with_cooldown.py
+++ 309.best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -4,21 +4,16 @@ from functools import cache
 class Solution:
     def maxProfit(self, prices: List[int]) -> int: # noqa
         self.prices = prices
-        return self.dp(0)
+        return self.dp(0, 0)
 
     @cache
-    def dp(self, index: int) -> int:
+    def dp(self, day: int, hold: bool) -> int:
         prices = self.prices
-        if len(prices) - index <= 1:
+        if day >= len(prices) and hold:
+            return float('-inf')
+        elif day >= len(prices) and not hold:
             return 0
-        prices = self.prices
-        low = prices[index]
-        max_profit = 0
-        for i in range(index, len(prices)):
-            p = prices[i]
-            if p <= low:
-                low = p
-            else:
-                profit = p - low + self.dp(i+2)
-                max_profit = max(max_profit, profit)
-        return max_profit
+        if hold:
+            return max(self.dp(day+2, False) + prices[day], self.dp(day+1, True))
+        else:
+            return max(self.dp(day+1, True) - prices[day], self.dp(day+1, False))
