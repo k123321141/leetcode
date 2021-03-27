02bbc91cbf924ad38def0e58b93c4005522b02ed
k123321141 <kk123321141@gmail.com>
2021-03-27 14:47:42 +0800

    6, 89%

--- 6.zigzag_conversion.py
+++ 6.zigzag_conversion.py
@@ -1,36 +1,22 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        '''
+    ['0', ' ', ' ', ' ', '8', ' ', ' ', ' ', '16', ' ', ' ', ' ', '24']
+    ['1', ' ', ' ', '7', '9', ' ', ' ', '15', '17', ' ', ' ', '23', '25']
+    ['2', ' ', '6', ' ', '10', ' ', '14', ' ', '18', ' ', '22', ' ']
+    ['3', '5', ' ', ' ', '11', '13', ' ', ' ', '19', '21', ' ', ' ']
+    ['4', ' ', ' ', ' ', '12', ' ', ' ', ' ', '20', ' ', ' ', ' ']
+        '''
+
         if len(s) == 1 or numRows == 1:
             return s
-        rows = [[] for i in range(numRows)]
-
-        y = 0
-        x = 0
-        is_zig = True
-        for i, w in enumerate(s):
-            if is_zig:
-                rows[y].append(w)
-                if y == numRows - 1:
-                    y -= 1
-                    x += 1
-                    is_zig = False
-                else:
-                    y += 1
-            else:
-                if y == 0:
-                    rows[y].append(w)
-                    y += 1
-                    is_zig = True
-                else:
-                    for j in range(numRows):
-                        if j == y:
-                            rows[j].append(w)
-                        else:
-                            rows[j].append(' ')
-                    y -= 1
-                    x += 1
-        ret = ''
-        for row in rows:
-            print(row)
-            ret += ''.join(row).replace(' ', '')
-        return ret
+        ret = []
+        step_size = (numRows-1)*2
+        for y in range(numRows):
+            for i in range(y, len(s), step_size):
+                ret.append(s[i])
+                if 0 < y < numRows-1:
+                    j = i + (step_size-2*y)
+                    if j < len(s):
+                        ret.append(s[j])
+        return ''.join(ret)
