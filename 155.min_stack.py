bd47cd74260a1b9b3cad7b0a54eee66fc37abbcd
k123321141 <kk123321141@gmail.com>
2022-07-18 12:02:23 +0800

    155, 99%, speed up by hash dict

--- 155.min_stack.py
+++ 155.min_stack.py
@@ -2,28 +2,31 @@ from collections import deque
 
 
 class MinStack:
+    """
+    maintain a dict to track min for given depth
+    """
 
     def __init__(self):
         self.Q = deque()
-        self.min = None
+        self.min_dict = {0: None}
 
     def push(self, val: int) -> None:
         pre_min = self.getMin()
+        self.Q.append(val)
+
         if pre_min is None or val < pre_min:
-            self.min = val
-        element = (val, pre_min)
-        self.Q.append(element)
+            self.min_dict[len(self.Q)] = val
+        else:
+            self.min_dict[len(self.Q)] = pre_min
 
     def pop(self) -> None:
-        val, pre_min = self.Q.pop()
-        self.min = pre_min
-        return val
+        return self.Q.pop()
 
     def top(self) -> int:
         if self.Q:
-            return self.Q[-1][0]
+            return self.Q[-1]
         else:
             return None
 
     def getMin(self) -> int:
-        return self.min
+        return self.min_dict[len(self.Q)]
