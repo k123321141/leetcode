b2768e2956842adf2ff459c4ec4cbdb4ca172f66
k123321141 <kk123321141@gmail.com>
2022-07-21 13:51:25 +0800

    130, 93%

--- 130.surrounded_regions.py
+++ 130.surrounded_regions.py
@@ -5,30 +5,46 @@ class Solution:
     def solve(self, board: List[List[str]]) -> None:  # noqa
         """
         Run BFS for each Os whether it is adjacent to boarder.
+        O(N)
         """
+        h = len(board)
+        w = len(board[0])
+        if h == 1 or w == 1:
+            return
 
         self.visited = set()
         self.board = board
-        for y, row in enumerate(board):
+
+        # check the boarder
+        for y in [0, h - 1]:
+            row = board[y]
             for x, v in enumerate(row):
                 if v == 'O' and (x, y) not in self.visited:
+                    self.visited.add((x, y))
                     self.bfs(x, y)
+        for x in [0, w - 1]:
+            for y in range(h):
+                v = board[y][x]
+                if v == 'O' and (x, y) not in self.visited:
+                    self.visited.add((x, y))
+                    self.bfs(x, y)
+
+        # flip all Os which not in visited
+        for y in range(1, h - 1):
+            for x in range(1, w - 1):
+                v = board[y][x]
+                if v == 'O' and (x, y) not in self.visited:
+                    board[y][x] = 'X'
 
     def bfs(self, x: int, y: int):
 
         h = len(self.board)
         w = len(self.board[0])
 
-        path = []
         Q = deque()
         Q.append((x, y))
-        boarder_flag = False
         while Q:
-            node = x, y = Q.popleft()
-            self.visited.add(node)
-            path.append(node)
-            if (y+1) == h or y == 0 or (x+1) == w or x == 0:
-                boarder_flag = True
+            x, y = Q.popleft()
 
             if y+1 < h and self.board[y+1][x] == 'O' and (x, y+1) not in self.visited:
                 Q.append((x, y+1))
@@ -42,7 +58,3 @@ class Solution:
             if x > 0 and self.board[y][x-1] == 'O' and (x-1, y) not in self.visited:
                 Q.append((x-1, y))
                 self.visited.add((x-1, y))
-        if not boarder_flag:
-            for x, y in path:
-                self.board[y][x] = 'X'
-    # [["O","O","O","O","O","O","O","O","X","O","O","O","O","O","X","O","O","O","O","O"],["O","O","O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O","O","O"],["X","O","O","X","O","X","O","O","O","O","X","O","O","X","O","O","O","O","O","O"],["O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","X","X","O"],["O","X","X","O","O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O"],["O","O","O","O","X","O","O","O","O","O","O","X","O","O","O","O","O","X","X","O"],["O","O","O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O","O","O"],["O","O","O","O","O","O","O","O","O","O","O","O","O","X","O","O","O","O","O","O"],["O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","X","O"],["O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O","O","O","O","O"],["O","O","O","O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O","O"],["O","O","O","O","X","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O"],["O","O","O","O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O","O"],["X","O","O","O","O","O","O","O","O","X","X","O","O","O","O","O","O","O","O","O"],["O","O","O","O","O","O","O","O","O","O","O","X","O","O","O","O","O","O","O","O"],["O","O","O","O","X","O","O","O","O","O","O","O","O","X","O","O","O","O","O","X"],["O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","X","O","X","O","O"],["O","X","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"],["O","O","O","O","O","O","O","O","X","X","O","O","O","X","O","O","X","O","O","X"],["O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"]]
