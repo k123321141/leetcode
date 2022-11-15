e588a4c18fb9abf0eb675a71b716c58d81e45615
k123321141 <kk123321141@gmail.com>
2022-11-15 13:33:26 +0800

    115, 95%

--- 115.distinct_subsequences.py
+++ 115.distinct_subsequences.py
@@ -2,8 +2,8 @@ from functools import cache
 
 
 class Solution:
-    @cache
     def numDistinct(self, s: str, t: str) -> int:
+
         if s == t:
             ret = 1
         elif len(t) > len(s):
@@ -14,19 +14,26 @@ class Solution:
                 if c == t[0]:
                     ret += 1
         else:
+            self.s = s
+            self.t = t
+            return self.dp(0, 0)
+        return ret
+
+    @cache
+    def dp(self, i: int, j: int) -> int:
+        s = self.s
+        t = self.t
+        x = s[i]
+        y = t[j]
+        if (len(s) - i) < (len(t) - j):
+            return 0
+        elif j == (len(t)-1):
             ret = 0
-            n = len(s)
-            for i in range(n):
-                if i == 0:
-                    sub = s[1:]
-                    ret += self.numDistinct(sub, t)
-                elif i == (len(s)-1):
-                    sub = s[:i]
-                    ret += 1 if sub == t else 0
-                else:
-                    left = s[:i]
-                    if t.startswith(left):
-                        right = s[i+1:]
-                        sub = left + right
-                        ret += self.numDistinct(right, t[len(left):])
+            for idx in range(i, len(s)):
+                ret += 1 if s[idx] == y else 0
+        else:
+            if x == y:
+                ret = self.dp(i+1, j+1) + self.dp(i+1, j)
+            else:
+                ret = self.dp(i+1, j)
         return ret
