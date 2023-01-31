e178707c66f804de51660e2e8c5559cb0cc0b4c2
k123321141 <kk123321141@gmail.com>
2023-01-31 18:21:40 +0800

    1218, 50%

---
+++ 1218.longest_arithmetic_subsequence_of_given_difference.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:  # noqa
+        jump_dict = {}
+        ret = 1
+        # O(N)
+        for i, v in enumerate(arr):
+            k = v - difference
+            if k in jump_dict:
+                jump_dict[v] = max(jump_dict[k] + 1, jump_dict.get(v, 1))
+                ret = max(ret, jump_dict[v])
+            else:
+                jump_dict[v] = 1
+        return ret
