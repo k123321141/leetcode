1ea55a9b3e9acefa6729f4a9e7f62391d7271a6a
k123321141 <kk123321141@gmail.com>
2023-11-28 21:10:30 +0800

    787, 5%

--- 787.cheapest_flights_within_k_stops.py
+++ 787.cheapest_flights_within_k_stops.py
@@ -1,105 +1,35 @@
-from collections import deque
 
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:  # noqa
-        '''Run an BFS without visited O(Nk), OOM
-        Try DFS
+        '''O(KE)
         '''
-        k = k + 1
-        if k <= 0:
-            return -1
-        G = {}
-        inv_G = {}
+
+        G = {i: [] for i in range(n)}
+
         for u, v, w in flights:
-            if u not in G:
-                G[u] = []
             G[u].append((v, w))
-
-            if v not in inv_G:
-                inv_G[v] = []
-            inv_G[v].append((u, w))
-
-        if k % 2 == 0:
-            src_path_dict = self.k_stops(G, src, k // 2)
-        else:
-            src_path_dict = self.k_stops(G, src, k // 2 + 1)
-
-        dst_path_dict = self.k_stops(inv_G, dst, k // 2)
-        min_cost = float('inf')
-        for mid, src_cost in src_path_dict.items():
-            if mid not in dst_path_dict:
-                continue
-            dst_cost = dst_path_dict[mid]
-            cost = src_cost + dst_cost
-            min_cost = min(min_cost, cost)
-        return min_cost if min_cost != float('inf') else -1
-
-    def k_stops(self, G, src, k):
-        path_dict = {src: 0}
-        if k == 0:
-            return path_dict
-        Q = deque()
-        if src in G:
-            for v, w in G[src]:
-                Q.append((v, w, 1))
-        while Q:
-            u, cost, depth = Q.popleft()
-            if path_dict.get(u, float('inf')) >= cost:
-                path_dict[u] = cost
-            if depth >= k:
-                continue
-            if u not in G:
-                continue
-            for v, w in G[u]:
-                new_cost = cost + w
-                Q.append((v, new_cost, depth + 1))
-        return path_dict
-
-    def dfs_k_stops(self, path_dict, G, src, k, depth):
+        cost_dict = {(src, i): 0 for i in range(k + 1)}
         for v, w in G[src]:
-            cost = path_dict[src] + w
-            if path_dict.get(v, float('inf')) >= cost:
-                path_dict[v] = cost
-            if depth < k:
-                self.dfs_k_stops(path_dict, G, v, k, depth + 1)
-
-        path_dict = {src: 0}
+            cost_dict[(v, 0)] = w
         if k == 0:
-            return path_dict
-        if src in G:
-            for v, w in G[src]:
-                Q.append((v, w, 1))
-        while Q:
-            u, cost, depth = Q.popleft()
-            if path_dict.get(u, float('inf')) >= cost:
-                path_dict[u] = cost
-            if depth >= k:
-                continue
-            if u not in G:
-                continue
-            for v, w in G[u]:
-                new_cost = cost + w
-                Q.append((v, new_cost, depth + 1))
-        return path_dict
+            return cost_dict.get((dst, 0), -1)
 
-    def _dfs(self, G, src, k):
-        path_dict = {src: 0}
-        if k == 0:
-            return path_dict
-        Q = deque()
-        if src in G:
-            for v, w in G[src]:
-                Q.append((v, w, 1))
-        while Q:
-            u, cost, depth = Q.popleft()
-            if path_dict.get(u, float('inf')) >= cost:
-                path_dict[u] = cost
-            if depth >= k:
-                continue
-            if u not in G:
-                continue
-            for v, w in G[u]:
-                new_cost = cost + w
-                Q.append((v, new_cost, depth + 1))
-        return path_dict
+        for i in range(1, k + 1):
+            # print(cost_dict)
+            for u, v, w in flights:
+                if (u, i - 1) in cost_dict:
+                    if (v, i) in cost_dict:
+                        cost_dict[(v, i)] = min(cost_dict[(v, i)], cost_dict[(u, i - 1)] + w)
+                    elif (v, i - 1) not in cost_dict:
+                        cost_dict[(v, i)] = cost_dict[(u, i - 1)] + w
+                    else:
+                        # print('------------')
+                        # print((v, i), ' = ', cost_dict[(v, i - 1)])
+                        # print((v, i), ' = ', cost_dict[(u, i - 1)] + w)
+                        cost_dict[(v, i)] = min(cost_dict[(v, i - 1)], cost_dict[(u, i - 1)] + w)
+        # print(cost_dict)
+        if (dst, k) in cost_dict:
+            return cost_dict[(dst, k)]
+        else:
+            return -1
