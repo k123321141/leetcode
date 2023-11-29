f031caf654552b38a403710cd288855dc32cee40
k123321141 <kk123321141@gmail.com>
2023-11-29 12:34:22 +0800

    787, 5% 2 direction

--- 787.cheapest_flights_within_k_stops.py
+++ 787.cheapest_flights_within_k_stops.py
@@ -1,3 +1,4 @@
+import math
 
 
 class Solution:
@@ -6,30 +7,44 @@ class Solution:
         '''
 
         G = {i: [] for i in range(n)}
-
         for u, v, w in flights:
             G[u].append((v, w))
-        cost_dict = {(src, i): 0 for i in range(k + 1)}
-        for v, w in G[src]:
-            cost_dict[(v, 0)] = w
-        if k == 0:
-            return cost_dict.get((dst, 0), -1)
 
-        for i in range(1, k + 1):
-            # print(cost_dict)
-            for u, v, w in flights:
-                if (u, i - 1) in cost_dict:
-                    if (v, i) in cost_dict:
-                        cost_dict[(v, i)] = min(cost_dict[(v, i)], cost_dict[(u, i - 1)] + w)
-                    elif (v, i - 1) not in cost_dict:
-                        cost_dict[(v, i)] = cost_dict[(u, i - 1)] + w
-                    else:
-                        # print('------------')
-                        # print((v, i), ' = ', cost_dict[(v, i - 1)])
-                        # print((v, i), ' = ', cost_dict[(u, i - 1)] + w)
-                        cost_dict[(v, i)] = min(cost_dict[(v, i - 1)], cost_dict[(u, i - 1)] + w)
-        # print(cost_dict)
-        if (dst, k) in cost_dict:
-            return cost_dict[(dst, k)]
-        else:
-            return -1
+        if k <= 1:
+            cost_dict = Solution.get_k_stops_cost(G, src, dst, k)
+            min_cost = cost_dict.get(dst, -1)
+            return min_cost if not math.isinf(min_cost) else -1
+
+        inv_G = {i: [] for i in range(n)}
+        for u, v, w in flights:
+            inv_G[v].append((u, w))
+        cost_dict = Solution.get_k_stops_cost(G, src, k // 2)
+        inv_cost_dict = Solution.get_k_stops_cost(inv_G, src, int(math.ceil(k / 2) - 1))
+
+        min_cost = float('inf')
+        for mid, w1 in cost_dict.items():
+            if mid in inv_cost_dict:
+                w2 = inv_cost_dict[mid]
+                min_cost = min(min_cost, w1 + w2)
+        # return 99
+        return min_cost if not math.isinf(min_cost) else -1
+
+    def get_k_stops_cost(G, src, k) -> dict:
+        pre_cost_dict = {}
+
+        cost_dict = {src: 0}
+        for v, w in G[src]:
+            cost_dict[v] = w
+        if k == 0:
+            return cost_dict
+
+        for _ in range(k):
+            pre_cost_dict, cost_dict = cost_dict, pre_cost_dict
+            for u in G:
+                for v, w in G[u]:
+                    cost_dict[v] = min(
+                        cost_dict.get(v, float('inf')),
+                        pre_cost_dict.get(u, float('inf')) + w,
+                        pre_cost_dict.get(v, float('inf')),
+                    )
+        return cost_dict
