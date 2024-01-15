1034a40792792ddc4a43cfb84bc15986700e4cb4
k123321141 <kk123321141@gmail.com>
2024-01-15 15:12:34 +0800

    2870, 5% replace divide op with minus

--- 2870.minimum_number_of_operations_to_make_array_empty.py
+++ 2870.minimum_number_of_operations_to_make_array_empty.py
@@ -1,5 +1,8 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:  # noqa
+        if len(nums) == 1:
+            return -1
+
         counter = {}
         err = 0
         ret = 0
@@ -7,12 +10,14 @@ class Solution:
             if n not in counter:
                 counter[n] = 0
             counter[n] += 1
+            if counter[n] > 6:
+                counter[n] -= 3
             if counter[n] == 1:
                 err += 1
             elif counter[n] == 2:
                 ret += 1
                 err -= 1
-            elif (counter[n] - 1) % 3 == 0:
+            elif counter[n] == 4:
                 ret += 1
         if err:
             return -1
